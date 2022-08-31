a6cad80f6bde47928c2c05987e28b8c5e7fbf758
ccarver80 <ckc_80@outlook.com>
2022-08-31 08:18:14 -0600

    download node-fetch and made test api call on api

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -5,7 +5,7 @@ export default function Header(props) {
   
     return (
        <div className="p-2 bg-slate-200 h-20 w-screen flex ">
-        <h1 className="mx-auto my-auto text-2xl">My Giphy Library</h1>
+        <h1 className="mx-auto my-auto text-2xl">My Reddit Application</h1>
         {props.userInfo ? ( 
             <>
             <h1 className="">Hello, {props.userInfo}</h1>
--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,10 +1,21 @@
 import Header from './Header'
 
 export default function Home() {
+
+    const submitSearch = async (e) => {
+        e.preventDefault(); 
+        fetch('http://localhost:5000/search')
+        .then(res => res.json())
+        .then(data => console.log(data))
+    }
+
     return (
         <>
-        
-        
+        <form onSubmit={submitSearch}>
+        <label>Search</label>
+        <input className="border border-black"/>
+        <button type='submit'>Go</button>
+        </form>
         </>
     )
 }
